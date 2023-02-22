be93517e7a5b3607e01ceee55a8f1efb87a15fa7
jjblancob <jjblancobautista@gmail.com>
2023-02-23 00:39:47 +0100

    create sesion 01 - tooling-vite

---
+++ ejemplo-vite/modules.js
@@ -0,0 +1,16 @@
+export const person = {
+    name: 'Juan Jesús',
+    surname:  'Blanco Bautista', 
+};
+
+// export const sayHello = () => {
+//     console.log('Hello');
+// };
+
+export function sayHello() {
+
+};
+
+const PROGRAMMING_LANGUAGE = 'JavaScript'
+
+export default PROGRAMMING_LANGUAGE;
